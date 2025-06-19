012eb7b5f4b500d5d26cfba8083e74dcb69a4b81
吟风抱月 <3472151649@qq.com>
2025-06-19 21:44:57 +0800

    推送测试

--- springai_chat/src/main/java/com/yyby/ai/chat/controller/ChatRoleController.java
+++ springai_chat/src/main/java/com/yyby/ai/chat/controller/ChatRoleController.java
@@ -2,7 +2,6 @@ package com.yyby.ai.chat.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
